ef8505b20b696ad5ec61724d53b19ab026ae43dc
Krzysztof Budnik <budnix@gmail.com>
2017-03-09 12:48:28 +0100

    Added support for removing 'import' expressions. #1

--- src/index.js
+++ src/index.js
@@ -27,6 +27,37 @@ export default function () {
             }
           }
         }
+      },
+
+      ImportDeclaration: {
+        enter(nodePath, { opts }) {
+          const extensionsInput = [].concat(opts.extensions || []);
+
+          if (extensionsInput.length === 0) {
+            return;
+          }
+          const extensions = extensionsInput.map(extFix);
+
+          if (extensions.indexOf(path.extname(nodePath.node.source.value)) > -1) {
+            const specifiers = nodePath.get('specifiers');
+
+            if (specifiers.length) {
+              const specifier = specifiers[specifiers.length - 1];
+
+              if (specifier.isImportDefaultSpecifier()) {
+                throw new Error(`${nodePath.node.source.value} should not be imported using default imports.`);
+              }
+              if (specifier.isImportSpecifier()) {
+                throw new Error(`${nodePath.node.source.value} should not be imported using named imports.`);
+              }
+              if (specifier.isImportNamespaceSpecifier()) {
+                throw new Error(`${nodePath.node.source.value} should not be imported using namespace imports.`);
+              }
+            }
+
+            nodePath.remove();
+          }
+        }
       }
     }
   };
